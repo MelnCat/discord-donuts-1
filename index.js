426d7775d029ffd39bafd93efa9e3bfd165ba4da
Yamboy1 <mbdolbel@gmail.com>
2018-10-02 20:24:01 +1300

    add todo

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const manager = new ShardingManager('./bot.js', { token: token, totalShards: 2 }
 manager.respawn = false
 
 manager.spawn()
+
+// TODO: Find out why this doesn't work
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}`))
 
 manager.on('message', console.log)
