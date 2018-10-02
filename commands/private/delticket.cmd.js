b949cac0174cdd1f3c87781acb482faa933c1173
Yamboy1 <mbdolbel@gmail.com>
2018-10-02 23:46:11 +1300

    fix up delticket

--- commands/private/delticket.cmd.js
+++ commands/private/delticket.cmd.js
@@ -7,8 +7,9 @@ module.exports = {
   permissions: canCook,
   description: 'Delete unworthy tickets, with a reason',
   async execute (message, args) {
+    if (!args) return message.reply('Please enter an order id')
     const deletedOrdersCount = await Orders.update({ status: 6 }, { where: { id: args.shift(), status: { [Op.lt]: 5 } }, individualHooks: true })
-    if (!deletedOrdersCount) message.reply('Couldn\'t find that order')
-    else message.reply('Order deleted')
+    if (deletedOrdersCount[0] < 1) return message.reply('Couldn\'t find that order')
+    message.reply('Order deleted')
   }
 }
