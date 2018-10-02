e3a77b9901e6300637af6eeb3dfb1619811d207e
Yamboy1 <mbdolbel@gmail.com>
2018-10-02 23:46:05 +1300

    implement cancel command

--- commands/public/cancel.cmd.js
+++ commands/public/cancel.cmd.js
@@ -1,1 +1,15 @@
-// TODO: Implement NEXT
+const { Orders, Op } = require('../../sequelize')
+
+const { everyone } = require('../../permissions')
+
+module.exports = {
+  name: 'cancel',
+  description: 'to cancel an order',
+  permissions: everyone,
+  async execute (message, args) {
+    const deletedOrdersCount = await Orders.update({ status: 6 }, { where: { user: message.author.id, status: { [Op.lt]: 5 } }, individualHooks: true })
+    if (deletedOrdersCount[0] < 1) return message.reply('You don\'t have any orders to cancel')
+    console.log(deletedOrdersCount)
+    message.reply('Order cancelled')
+  }
+}
