623acc0b8dbbce8269e18c9e37d32e8d6e35aee0
Yamboy1 <mbdolbel@gmail.com>
2018-10-03 00:41:40 +1300

    implement invite command

--- commands/public/invite.cmd.js
+++ commands/public/invite.cmd.js
@@ -1,1 +1,10 @@
-// TODO: Implement
+const { everyone } = require('../../permissions')
+
+module.exports = {
+  name: 'invite',
+  description: 'the invite code for the bot',
+  permissions: everyone,
+  execute (message, args, client) {
+    message.channel.send('Invite me here :arrow_forward: https://donuts.its-mustard.me')
+  }
+}
