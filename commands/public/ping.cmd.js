0649c871fe9d9df77437519fdb31b630399ab603
Yamboy1 <mbdolbel@gmail.com>
2018-10-03 00:52:54 +1300

    implement ping and wping commands

--- commands/public/ping.cmd.js
+++ commands/public/ping.cmd.js
@@ -1,1 +1,9 @@
-// TODO: Implement
+module.exports = {
+  name: 'ping',
+  description: 'ping...',
+  async execute (message, args, client) {
+    const startTime = Date.now()
+    const pingMsg = await message.channel.send('Pinging...')
+    pingMsg.edit(`:ping_pong: Pong! Took \`${Math.round(Date.now() - startTime)}ms\`!`)
+  }
+}
--- commands/public/wping.cmd.js
+++ commands/public/wping.cmd.js
@@ -1,1 +1,10 @@
-// TODO: Implement
+const { everyone } = require('../../permissions')
+
+module.exports = {
+  name: 'wping',
+  description: 'webhook ping...', // TODO: Shouldn't this be websocket?
+  permissions: everyone,
+  async execute (message, args, client) {
+    message.channel.send(`:ping_pong: Pong! Took \`${message.client.ping.toFixed(0)}ms\`!`)
+  }
+}
