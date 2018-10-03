df70704f7965e61cfd96883d83775284bc2c5fb7
Yamboy1 <mbdolbel@gmail.com>
2018-10-03 15:08:20 +1300

    implement servers command

--- commands/public/servers.cmd.js
+++ commands/public/servers.cmd.js
@@ -1,1 +1,29 @@
-// TODO: Implement
+const Discord = require('discord.js')
+
+const { everyone } = require('../../permissions')
+
+module.exports = {
+  name: 'servers',
+  description: 'Info about the bots servers and shards',
+  permissions: everyone,
+  /**
+   *
+   * @param { Discord.Message } message The message with the command
+   * @param { [String] } args The arguments to the command
+   * @param { Discord.Client } client The client instance
+   */
+  async execute (message, args, client) {
+    // FIXME: Need better wording
+    const embed =
+      new Discord.MessageEmbed()
+        .setTitle('Number of servers')
+        .setDescription('Information on the number of servers / shards of this bot')
+        .addField('Total servers', await (client.shard.broadcastEval('this.guilds.size')
+          .then(arr => arr.reduce((acc, x) => acc + x))
+          .catch(console.log)))
+        .addField('Current shard', `Shard ${client.shard.id}`)
+        .addField('Servers on current shard', client.guilds.size)
+
+    message.channel.send(embed)
+  }
+}
