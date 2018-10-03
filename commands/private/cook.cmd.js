f04aac4763b5d394546a6789c046dd8e7ba006d3
Yamboy1 <mbdolbel@gmail.com>
2018-10-03 15:22:22 +1300

    add todos

--- commands/private/cook.cmd.js
+++ commands/private/cook.cmd.js
@@ -12,13 +12,15 @@ module.exports = {
     const id = args.shift()
     const order = await Orders.findOne({ where: { id: id, claimer: message.author.id } })
     if (!order) return message.reply('Either this order doesn\'t exist, or you haven\'t claimed it')
-    await message.channel.send('The next message you send will be set as the order\'s image Only URLs are supported atm :cry')
+    // TODO: Support non URL image
+    await message.channel.send('The next message you send will be set as the order\'s image Only URLs are supported atm :cry:')
     const response = await message.channel.awaitMessages(m => m.author.id === order.get('claimer'), { max: 1, time: 30000 })
     if (!response.size) return message.reply('You didn\'t respond in time')
     try {
       await Orders.update({ status: 2, url: response.first().content }, { where: { id: id }, individualHooks: true })
     } catch (e) {
       if (e.name === 'SequelizeValidationError') {
+        // TODO: Add better error detection
         return message.channel.send('That doesn\'t look like a URL to me')
       }
     }
