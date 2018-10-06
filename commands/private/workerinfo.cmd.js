19e6dfe89ea20527af3c2030cf02e4245631e81b
Hunter Parcells <hparcells@icloud.com>
2018-10-06 14:58:59 -0400

    Worker Info Command

---
+++ commands/private/workerinfo.cmd.js
@@ -0,0 +1,49 @@
+const DDEmbed = require("../../structures/DDEmbed.struct");
+
+const { canCook } = require("../../permissions");
+const { WorkerInfo } = require("../../sequelize");
+
+module.exports = {
+	name: "workerinfo",
+	permissions: canCook,
+	description: "View a worker's stats.",
+	async execute(message, args, client) {
+		if (!args.shift()) {
+			let worker = await WorkerInfo.findOne({ where: { id: message.author.id } });
+
+			const embed = new DDEmbed(client)
+				.setStyle("white")
+				.setTitle(`Worker Info for ${message.author.username}+${message.author.discriminator}`)
+				.addField("Orders Cooked", worker.cooks)
+				.addField("Orders Delivered", worker.delivers)
+				.addField("Last Cook", timeAgoToString(worker.lastCook))
+				.addField("Last Deliver", timeAgoToString(worker.lastDeliver));
+
+			message.channel.send(embed);
+		} else {
+			let worker = args[0].replace("<", "");
+			worker = worker.replace("!", "");
+			worker = worker.replace("@", "");
+			worker = worker.replace(">", "");
+
+			const embed = new DDEmbed(client)
+				.setStyle("white")
+				.setTitle(`Worker Info for ${client.users.get(worker).username}+${client.users.get(worker).discriminator}`)
+				.addField("Orders Cooked", worker.cooks)
+				.addField("Orders Delivered", worker.delivers)
+				.addField("Last Cook", timeAgoToString(worker.lastCook))
+				.addField("Last Deliver", timeAgoToString(worker.lastDeliver));
+
+			message.channel.send(embed);
+		}
+	},
+};
+
+function timeAgoToString(timestamp) {
+	let timeAgo = Date.now() - timestamp;
+	let minutes = parseInt((timeAgo / (1000 * 60)));
+	let hours = parseInt((timeAgo / (1000 * 60 * 60)));
+	let days = parseInt((timeAgo / (1000 * 60 * 60 * 24)));
+
+	return `${days} Days, ${hours} Hours, and ${minutes} Minutes Ago.`;
+}
