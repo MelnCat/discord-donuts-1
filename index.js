05f2219a9a55e2df330f250cf14e18f36e7e7043
Yamboy1 <16280@grammar.net.nz>
2018-10-09 08:49:15 +1300

    update discord.js version, and fix problem

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const { token } = require("./auth.json");
 
 const manager = new ShardingManager("./bot.js", { token: token, totalShards: 2 });
 
-manager.spawn();
-
-// TODO: Find out why this doesn't work
 manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}.`));
+
+manager.spawn();
 
 manager.on("message", console.log);
