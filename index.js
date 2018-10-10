f22dedee2d1f6c7c9b0b138b1edd428fed0071fd
Yamboy1 <16280@grammar.net.nz>
2018-10-11 05:21:48 +1300

    improve error detetction

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+process.on("unhandledRejection", console.log);
+process.on("unhandledRejection", console.log);
+
 const { ShardingManager } = require("discord.js");
 
 const { token } = require("./auth.json");
@@ -6,6 +9,6 @@ const manager = new ShardingManager("./bot.js", { token: token, totalShards: 2 }
 
 manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}.`));
 
-manager.spawn();
-
 manager.on("message", console.log);
+
+manager.spawn();
