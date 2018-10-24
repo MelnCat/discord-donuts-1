1e51101ebdd5b06cc3a7727ee4fc739424efbd6e
Yamboy1 <16280@grammar.net.nz>
2018-10-24 21:26:11 +1300

    add category attribute, needs docs

--- structures/DDCommand.struct.js
+++ structures/DDCommand.struct.js
@@ -38,6 +38,12 @@ class DDCommand {
 		this.permissions = permissionFunction;
 		return this;
 	}
+
+	setCategory(category) {
+		this.category = category;
+		return this;
+	}
+
 	/**
 	 *
 	 * @callback ExecFunction
@@ -74,10 +80,15 @@ class DDCommand {
 
 	/**
 	 * Gets the permissions required to run the command
+	 * @param { Discord.GuildMember } member The member to test the permissions on
 	 * @returns { PermissionFunction | TypeError }
 	 */
-	getPermissions() {
-		return this.permissions || new TypeError("No permissions have been set for this command");
+	getPermissions(member) {
+		return this.permissions(member) || new TypeError("No permissions have been set for this command");
+	}
+
+	getCategory() {
+		return this.description || new TypeError("A category has not been specified for this command");
 	}
 
 	/**
