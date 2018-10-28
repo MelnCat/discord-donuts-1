77cd7e31a5baa54aeebb41267b28da2e82c0dc43
Hunter Parcells <hparcells@icloud.com>
2018-10-28 13:51:05 -0400

    Upvote Command

---
+++ commands/public/upvote.cmd.js
@@ -0,0 +1,21 @@
+const DDEmbed = require("../../structures/DDEmbed.struct");
+const DDCommand = require("../../structures/DDCommand.struct");
+
+const { everyone } = require("../../permissions");
+
+module.exports =
+	new DDCommand()
+		.setName("upvote")
+		.setDescription("The link to upvote the bot.")
+		.setPermissions(everyone)
+		.setFunction(async(message, args, client) => {
+			const embed =
+				new DDEmbed(client)
+					.setStyle("colorful")
+					.setTitle("Upvote")
+					.setDescription("Spread the joy of donuts and upvote us!")
+					.addField("Upvote Link", "https://its-mustard.me/upvote")
+					.setThumbnail("https://images.emojiterra.com/twitter/512px/1f517.png");
+
+			message.channel.send(embed);
+		});
