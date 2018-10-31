07596b69270402a4d5d7c1980bf902c7ce257814
Yamboy1 <16280@grammar.net.nz>
2018-11-01 09:41:08 +1300

    addawait

--- commands/private/restart.cmd.js
+++ commands/private/restart.cmd.js
@@ -11,7 +11,7 @@ module.exports =
 		.setPermissions(isBotOwner)
 		.setFunction(async(message, args, client) => {
 			await message.reply("Are you sure you would like to restart the bot? Type yes to continue.");
-			const responses = message.channel.awaitMessages(m => m.content === "yes", { max: 1, time: 10000, errors: ["time"] })
+			const responses = await message.channel.awaitMessages(m => m.content === "yes", { max: 1, time: 10000, errors: ["time"] })
 
 			if (responses.size > 0) return process.exit();
 
