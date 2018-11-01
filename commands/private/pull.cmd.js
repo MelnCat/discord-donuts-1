4fcd7bdbd1562a48573826262bf3ea57cee4e8b6
Yamboy1 <16280@grammar.net.nz>
2018-11-01 16:13:55 +1300

    Add pull command

---
+++ commands/private/pull.cmd.js
@@ -0,0 +1,22 @@
+const { timeout } = require("../../helpers");
+const { isBotOwner } = require("../../permissions");
+const { pullScript } = require("../../auth.json");
+
+const DDEmbed = require("../../structures/DDEmbed.struct");
+const DDCommand = require("../../structures/DDCommand.struct");
+
+const { execFileSync } = require("child_process");
+
+module.exports =
+	new DDCommand()
+		.setName("pull")
+		.setDescription("Pulls changes to the bot")
+		.setPermissions(isBotOwner)
+		.setFunction(async(message, args, client) => {
+			await message.reply("Are you sure you would like to pull changes? Type yes to continue.");
+			const responses = await message.channel.awaitMessages(m => m.content === "yes", { max: 1, time: 10000, errors: ["time"] });
+
+			if (responses.size > 0) return message.channel.send(execFileSync(pullScript), { code: "bash" });
+
+			message.reply("Pull cancelled.");
+		});
