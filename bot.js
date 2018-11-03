33c89b628eeecd8198b72b79b61920052b8509e7
TNThacker2015 <37024464+TNThacker2015@users.noreply.github.com>
2018-11-03 13:32:52 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,8 +60,9 @@ ${commit}\`\`\`
 
 client.on("message", async message => {
 	const gprefix = await Prefixes.findById(message.guild.id);
-	if (!gprefix) gprefix = prefix;
-	gprefix = gprefix.prefix;
+	let gprefixstr
+	if (!gprefix) gprefixstr = prefix;
+	gprefixstr = gprefix.prefix;
 	if (![prefix, `<@${client.id}>`, gprefix].some(x => message.content.startsWith(x)) || message.author.bot) return;
 
 	if (await Blacklist.findById(message.author.id)) return message.channel.send("I apologize, but you've been blacklisted from this bot!");
