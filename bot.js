4f41c9ee0a9732f4e8cb82d3dc596e58eaf882d3
TNThacker2015 <37024464+TNThacker2015@users.noreply.github.com>
2018-11-03 13:30:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const glob = require("glob");
 
 const DDClient = require("./structures/DDClient.struct");
 
-const { Orders, Blacklist, WorkerInfo, PrecookedDonuts, Op } = require("./sequelize");
+const { Orders, Blacklist, WorkerInfo, PrecookedDonuts, Op, Prefixes } = require("./sequelize");
 const { token, prefix, channels: { ticketChannel, guildLogChannel, testChannel } } = require("./auth.json");
 const { generateTicket, timeout, updateWebsites, messageAlert, checkOrders } = require("./helpers");
 
@@ -59,7 +59,10 @@ ${commit}\`\`\`
 });
 
 client.on("message", async message => {
-	if (![prefix, `<@${client.id}>`].some(x => message.content.startsWith(x)) || message.author.bot) return;
+	const gprefix = await Prefixes.findById(message.guild.id);
+	if (!gprefix) gprefix = prefix
+	gprefix = gprefix.prefix
+	if (![prefix, `<@${client.id}>`, gprefix].some(x => message.content.startsWith(x)) || message.author.bot) return;
 
 	if (await Blacklist.findById(message.author.id)) return message.channel.send("I apologize, but you've been blacklisted from this bot!");
 	if (await Blacklist.findById(message.guild.id)) {
