8ad04d2d99c67c8409d6121856c39298226c2ce0
Beta <21072460+bvta@users.noreply.github.com>
2018-11-04 18:05:44 -0600

    Fixed some grammatical errors.

--- commands/private/cook.cmd.js
+++ commands/private/cook.cmd.js
@@ -13,15 +13,15 @@ module.exports =
 		.setDescription("Use this to cook donuts.")
 		.setPermissions(canCook)
 		.setFunction(async(message, args, client) => {
-			if (!args[0]) return message.reply("Please provide an id to cook");
-			if (!args[0].match(/^0[a-zA-Z0-9]{6}$/)) return message.reply("Please provide a valid id");
+			if (!args[0]) return message.reply("Please provide an ID to a valid ticket.");
+			if (!args[0].match(/^0[a-zA-Z0-9]{6}$/)) return message.reply("That ID isn't valid, please try again.");
 
 			const order = await Orders.findById(args[0]);
 			if (!order) return message.reply("That order doesn't exist");
-			if (order.claimer !== message.author.id) return message.reply("You haven't claimed this order");
+			if (order.claimer !== message.author.id) return message.reply("You are unauthorized to access this order. This may be due to you not being the baker.");
 
-			if (order.status === 2) return message.reply("That order is currently cooking");
-			if (order.status > 2) return message.reply("That order has already been cooked");
+			if (order.status === 2) return message.reply("That order is currently being cooked, please check back later.");
+			if (order.status > 2) return message.reply("That order has completed cooking.");
 
 			let url;
 
@@ -43,7 +43,7 @@ module.exports =
 					new DDEmbed(client)
 						.setStyle("white")
 						.setTitle("Cook")
-						.setDescription("The next message you send will be set as the order's image");
+						.setDescription("The next message you send will be set as the order's image.");
 
 				await message.channel.send(urlEmbed);
 
@@ -51,7 +51,7 @@ module.exports =
 					m => m.author.id === order.claimer,
 					{ max: 1, time: 30000 });
 
-				if (!response.size) return message.reply("You didn't respond in time, cancelling cook");
+				if (!response.size) return message.reply("Cook sequence failed, please try again.");
 
 				if (isurl(response.first().content.trim())) url = response.first().content.trim();
 				else if (response.first().attachments.size) url = response.first().attachments.first().url;
@@ -81,7 +81,7 @@ module.exports =
 				.setThumbnail("https://images.emojiterra.com/twitter/512px/2705.png");
 
 			await message.channel.send(cookEmbed);
-			await client.users.get(order.user).send(`:thumbsup: Your cook, ${client.users.get(order.claimer).tag}, just put your ticket in the oven! It should take **3 minutes** to cook!`);
+			await client.users.get(order.user).send(`:thumbsup: Your cook, ${client.users.get(order.claimer).tag}, just put your order into the oven! It should take **3 minutes** to cook!`);
 			const workerraw = await WorkerInfo.findOrCreate({ where: { id: message.author.id }, defaults: { id: message.author.id, cooks: 0, delivers: 0, lastCook: 0, lastDeliver: 0 } });
 			const worker = workerraw[0];
 			await worker.update({ cooks: worker.cooks + 1, lastCook: Date.now() });
