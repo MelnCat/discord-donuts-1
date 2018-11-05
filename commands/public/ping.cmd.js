4d7227e29fcb8f8f42a53a86c6eb1c470a2d31ce
Beta <21072460+bvta@users.noreply.github.com>
2018-11-04 18:24:38 -0600

    Update ping.cmd.js

--- commands/public/ping.cmd.js
+++ commands/public/ping.cmd.js
@@ -4,24 +4,14 @@ const DDCommand = require("../../structures/DDCommand.struct");
 const { everyone } = require("../../permissions");
 
 module.exports =
-	new DDCommand()
-		.setName("ping")
-		.setDescription("The bot ping.")
-		.setPermissions(everyone)
-		.setFunction(async(message, args, client) => {
-			const startTime = Date.now();
-
-			const loadingEmbed =
-				new DDEmbed(client)
-					.setStyle("colorful")
-					.setDescription("Pinging..")
-
-			const pingMessage = await message.channel.send(loadingEmbed);
-
-			const finishedEmbed =
-				new DDEmbed(client)
-					.setStyle("colorful")
-					.setDescription(`:ping_pong: Pong! Took \`${Math.round(Date.now() - startTime)} ms\`!`)
-
-			pingMessage.edit(finishedEmbed);
-		});
+   new DDCommand()
+      .setName("ping")
+      .setDescription("Fetches time stamp values.")
+      .setPermissions(everyone)
+      .setFunction(async (message, args, client) => {
+         const startTime = Date.now();
+         const m = await message.channel.send("Fetching time stamp values..");
+         const endTime = Date.now();
+         await m.edit(`Message: \`${endTime - startTime}ms\`\nDiscord Heartbeat: \`${Math.round(this.client.ws.ping)}ms\``);
+         return;
+      });
