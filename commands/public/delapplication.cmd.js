2479d07437892e7c0a4d5d79eb362bec28fefa23
RaymondW <tnthacker2015@gmail.com>
2018-11-07 21:16:29 -0800

    commit

--- commands/public/delapplication.cmd.js
+++ commands/public/delapplication.cmd.js
@@ -3,24 +3,24 @@ const DDCommand = require("../../structures/DDCommand.struct");
 const { channels: { applicationChannel } } = require("../../auth");
 const { everyone } = require("../../permissions");
 const { Applications } = require("../../sequelize");
-const stringSimilarity = require('string-similarity');
+const stringSimilarity = require("string-similarity");
 module.exports =
 	new DDCommand()
-        .setName("delapplication")
-        .addAliases("delapply", "del-apply", "del-application", "delete-apply", "deleteapply")
+		.setName("delapplication")
+		.addAliases("delapply", "del-apply", "del-application", "delete-apply", "deleteapply")
 		.setDescription("Delete your application if you have one.")
 		.setPermissions(everyone)
 		.setFunction(async(message, args, client) => {
 			if (!await Applications.findById(message.author.id)) return message.channel.send("<:no:501906738224562177> You do not have an application.");
-            const questions = client.questions.splice(0, client.questions.length-1)
-            const app = await Applications.findById(message.author.id)
-            await app.destroy()
-            const embed =
+			const questions = client.questions.splice(0, client.questions.length-1)
+			const app = await Applications.findById(message.author.id)
+			await app.destroy()
+			const embed =
 				new DDEmbed(client)
 					.setStyle("colorful")
 					.setTitle(`Deleted Application from ${message.author.tag}!`)
 					.setThumbnail("https://cdn.discordapp.com/attachments/491045091801300992/509907961272074270/news.png")
-                    .setDescription("They have deleted their application.");
+					.setDescription("They have deleted their application.");
 			message.channel.send("This is the end your our application. Thanks for applying! We will get back to you ASAP! Remember to join our server, our invite is https://discord.gg/WJgamKm. Remember that improper or incomplete applications will not be considered, and asking when you're gonna be hired or inquiring about your application in any way before three days of your submission date is against our rules!");
 			await Applications.create({ id: message.author.id, application: JSON.stringify(responses) });
 			await client.channels.get(applicationChannel).send(embed);
--- commands/public/editapplication.cmd.js
+++ commands/public/editapplication.cmd.js
@@ -3,44 +3,44 @@ const DDCommand = require("../../structures/DDCommand.struct");
 const { channels: { applicationChannel } } = require("../../auth");
 const { everyone } = require("../../permissions");
 const { Applications } = require("../../sequelize");
-const stringSimilarity = require('string-similarity');
+const stringSimilarity = require("string-similarity");
 module.exports =
 	new DDCommand()
-        .setName("editapplication")
-        .addAliases("editapply", "edit-apply", "edit-application")
+		.setName("editapplication")
+		.addAliases("editapply", "edit-apply", "edit-application")
 		.setDescription("Edit your application if you have one.")
 		.setPermissions(everyone)
 		.setFunction(async(message, args, client) => {
-            async function getReactions(display, reactions) {
-                const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === message.author.id
-                let msg = await message.channel.send(display);
-                for (const r of reactions) {
-                    msg.react(r)
-                }
-                const col = await msg.awaitReactions(filter, { time: 15000, max: 1 })
-                if (!col.size) return message.channel.send("You did not react to the message so I ended this session.")
-                return reactions.indexOf(col.first().emoji.name)
-            }
+			async function getReactions(display, reactions) {
+				const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === message.author.id
+				let msg = await message.channel.send(display);
+				for (const r of reactions) {
+					msg.react(r)
+				}
+				const col = await msg.awaitReactions(filter, { time: 15000, max: 1 })
+				if (!col.size) return message.channel.send("You did not react to the message so I ended this session.")
+				return reactions.indexOf(col.first().emoji.name)
+			}
 			if (!await Applications.findById(message.author.id)) return message.channel.send("<:no:501906738224562177> You do not have an application.");
-            const questions = client.questions.splice(0, client.questions.length-1)
-            const app = await Applications.findById(message.author.id)
-            if (!args[0]) return message.channel.send("Please specify which value you want to change. Example: `d!editapply 5 15`")
-            if (!args[1]) return message.channel.send("Please specify what you want to change the value into. Example: `d!editapply 5 15`")
-            const content = args.splice(1, args.length).join(" ")
-            const apparray = JSON.parse(app.application);
-            const change = stringSimilarity.findBestMatch(content, questions).bestMatch.target
-            const index = questions.indexOf(change)
-            const sel = await getReactions(`Do you want to change "${change}" to "${content}"?`, ["❌", "✅"])
-            if (sel === 0) return message.channel.send("Ok, I cancelled this session!")
-            let updated = apparray
-            updated[index] = content
-            app.update({application: updated})
-            const embed =
+			const questions = client.questions.splice(0, client.questions.length-1)
+			const app = await Applications.findById(message.author.id)
+			if (!args[0]) return message.channel.send("Please specify which value you want to change. Example: `d!editapply 5 15`")
+			if (!args[1]) return message.channel.send("Please specify what you want to change the value into. Example: `d!editapply 5 15`")
+			const content = args.splice(1, args.length).join(" ")
+			const apparray = JSON.parse(app.application);
+			const change = stringSimilarity.findBestMatch(content, questions).bestMatch.target
+			const index = questions.indexOf(change)
+			const sel = await getReactions(`Do you want to change "${change}" to "${content}"?`, ["❌", "✅"])
+			if (sel === 0) return message.channel.send("Ok, I cancelled this session!")
+			let updated = apparray
+			updated[index] = content
+			app.update({application: updated})
+			const embed =
 				new DDEmbed(client)
 					.setStyle("colorful")
 					.setTitle(`Changed Application for ${message.author.tag}!`)
 					.setThumbnail("https://cdn.discordapp.com/attachments/491045091801300992/509907961272074270/news.png")
-                    .setDescription("They have edited their application.")
-                    .addField(change, content);
+					.setDescription("They have edited their application.")
+					.addField(change, content);
 			await client.channels.get(applicationChannel).send(embed);
 		});
