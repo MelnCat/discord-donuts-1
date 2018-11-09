3c5c456b730df5e52a2bc556d4a94e8fe5fc861a
RaymondW <tnthacker2015@gmail.com>
2018-11-08 18:20:03 -0800

    commit

--- commands/private/leaderboard.cmd.js
+++ commands/private/leaderboard.cmd.js
@@ -13,9 +13,11 @@ module.exports =
 		.setFunction(async(message, args, client) => {
 			let sel = args[2]?args[2]:"all"
 			if (args[2] && !["all", "cooks", "delivers"].includes(sel)) return message.channel.send("The filter must be 'cooks', 'delivers' or 'all'.")
-			let order = {"all": [[sequelize.fn('SUM', sequelize.col('cooks'), sequelize.col('delivers')), "DESC"]], "cooks": [["cooks", "DESC"]], "delivers": [["delivers", "DESC"]]}[sel]
+			//let order = {"all": [[sequelize.fn('SUM', sequelize.col('cooks'), sequelize.col('delivers')), "DESC"]], "cooks": [["cooks", "DESC"]], "delivers": [["delivers", "DESC"]]}[sel]
+			let order = {"all": "cooks + delivers", "cooks": "cooks", "delivers": "delivers"}[sel]
 			let start = !isNaN(args[0])?Number(args[0])-1:0
-			const ordered = await WorkerInfo.findAll({order: order});
+			//const ordered = await WorkerInfo.findAll({order: order});
+			const ordered = await sequelize.query("SELECT * FROM `workerinfos` ORDER BY "+order+" DESC", { type: sequelize.QueryTypes.SELECT});
 			const mapped = ordered.map(x=>[x.cooks, x.delivers, client.users.get(x.id)?client.users.get(x.id).tag:x.username]);
 			let end =  !isNaN(args[1])?Number(args[1]):ordered.length<10?ordered.length:9
 			if (start > end || start > ordered.length || end > ordered.length ) {
