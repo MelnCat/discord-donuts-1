8f5b27987d73a39920ec72ed40987685288c4ec4
RaymondW <tnthacker2015@gmail.com>
2018-11-08 21:06:24 -0800

    commit

--- commands/public/apply.cmd.js
+++ commands/public/apply.cmd.js
@@ -15,8 +15,8 @@ module.exports =
 			const lastmsg = "Last but not least, please ensure that you comply with the following:\nDo you recognize by penalty of perjury that all information presented is 100% genuine, that you stated the truth and nothing but the truth, and that we can hold you accountable? Do you acknowledge that you have read your obligations accurately and thoroughly and will be held responsible for your efforts, notwithstanding of the situation and/or outcome? Do you agree already be following, to continue following and sustain Discord's Terms of Service (found at https://discordapp.com/tos), Community Guidelines (found at https://discordapp.com/guidelines) and the rules of our Discord server; and are amenable to face the consequences to the highest extent if you are affirmed guilty of unlawful actions, per these three documents?";
 			message.channel.send("Thank you for applying! We will take you through a couple of questions. Please read through the whole application clearly and answer all the questions to the best of your ability. Take your time.\n**PREREQUISITES**:\nPlease be in our server, not be banned from our server and/or blacklisted from the bot in any previous or current occasion (with exceptions) and have a clean/somewhat clean record.");
 			async function getMessage(display) {
-				message.author.send(display);
-				let v = await message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, time: 17000 });
+				let m = message.author.send(display);
+				let v = await m.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, time: 17000 });
 				if (v.size === 0) return message.author.send("You did not provide me with a value so I cancelled this session.");
 				let vv = v.first().content;
 				return vv;
@@ -42,7 +42,7 @@ module.exports =
 				if (question.length > 255) resp = `${resp.substr(0, 251)}...`;
 				embed.addField(question, resp);
 			}
-			message.channel.send("This is the end your our application. Thanks for applying! We will get back to you ASAP! Remember to join our server, our invite is https://discord.gg/WJgamKm. Remember that improper or incomplete applications will not be considered, and asking when you're gonna be hired or inquiring about your application in any way before three days of your submission date is against our rules!");
+			message.author.send("This is the end your our application. Thanks for applying! We will get back to you ASAP! Remember to join our server, our invite is https://discord.gg/WJgamKm. Remember that improper or incomplete applications will not be considered, and asking when you're gonna be hired or inquiring about your application in any way before three days of your submission date is against our rules!");
 			message.author.send(`Thank you for applying. Your application code is \`${message.author.id.substr(8, 15)}\`. Thank you!`);
 			await Applications.create({ id: message.author.id, application: JSON.stringify(responses), code: message.author.id.substr(8, 15) });
 			await client.channels.get(applicationChannel).send(embed);
