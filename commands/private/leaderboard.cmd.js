b09c45aeba8e14a67fd842a080a5e3abe6407bf6
RaymondW <tnthacker2015@gmail.com>
2018-11-08 17:06:11 -0800

    commit

---
+++ commands/private/leaderboard.cmd.js
@@ -0,0 +1,39 @@
+const DDEmbed = require("../../structures/DDEmbed.struct");
+const DDCommand = require("../../structures/DDCommand.struct");
+
+const { canCook } = require("../../permissions");
+const { WorkerInfo } = require("../../sequelize");
+
+module.exports =
+	new DDCommand()
+		.setName("leaderboard")
+		.addAliases("leaderboards", "top", "lb")
+		.setDescription("Checks the overall leaderboard..")
+		.setPermissions(canCook)
+		.setFunction(async(message, args, client) => {
+			let sel = args[2]?args[2]:"all"
+			if (args[2] && !["all", "cooks", "delivers"].includes(sel)) return message.channel.send("The filter must be 'cooks', 'delivers' or 'all'.")
+			let order = {"all": ['id', 'DESC'], "cooks": [['cooks', 'DESC']], "delivers": [['delivers', 'DESC']]}
+			let start = !isNaN(args[0])?Number(args[0])-1:0
+			const ordered = await WorkerInfo.findAll({order: [order]});
+			const mapped = ordered.map(x=>[x.cooks, x.delivers, client.users.get(x.id)?client.users.get(x.id).tag:x.username]);
+			let end =  !isNaN(args[1])?Number(args[1])-1:ordered.length<10?ordered.length:9
+			if (start > end || start > ordered.length || end > ordered.length ) {
+				return message.channel.send("Selection not in range.")
+			}
+			if (end - start > 19) return message.channel.send("Please make the number of results less than 20.")
+			const sliced = mapped.slice(start, end)
+			const embed =
+				new DDEmbed(client)
+					.setStyle("colorful")
+					.setTitle(`The overall worker leaderboard.`)
+					.setDescription(`Showing ${start+1} to ${end+1}. Filter: ${sel}.`)
+					.setThumbnail("https://images.emojiterra.com/twitter/512px/1f3d3.png");
+			sliced.forEach(v=> {
+				const i = sliced.indexOf(v)
+				const places = {0: "🥇", 1: "🥈", 2: "🥉"}
+				const pre = places[i]?places[i]:""
+				embed.addField(`${pre} #${i+1}: ${v[2]}`, `${v[0]} cooks and ${v[1]} delivers. ${v[0] + v[1]} total.`)
+			})
+			message.channel.send(embed);
+		});
