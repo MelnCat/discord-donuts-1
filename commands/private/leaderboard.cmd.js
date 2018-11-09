d70779c7a603843134dbb55e13dd07b4c1ea8196
codefactor-io <support@codefactor.io>
2018-11-09 02:25:33 +0000

    [CodeFactor] Apply fixes to commit 84e1e3b

--- commands/private/leaderboard.cmd.js
+++ commands/private/leaderboard.cmd.js
@@ -11,17 +11,17 @@ module.exports =
 		.setDescription("Checks the overall leaderboard..")
 		.setPermissions(canCook)
 		.setFunction(async(message, args, client) => {
-			let sel = args[2]?args[2]:"all"
-			if (args[2] && !["all", "cooks", "delivers"].includes(sel)) return message.channel.send("The filter must be 'cooks', 'delivers' or 'all'.")
-			//let order = {"all": [[sequelize.fn('SUM', sequelize.col('cooks'), sequelize.col('delivers')), "DESC"]], "cooks": [["cooks", "DESC"]], "delivers": [["delivers", "DESC"]]}[sel]
-			let order = {"all": "cooks + delivers", "cooks": "cooks", "delivers": "delivers"}[sel]
-			let start = !isNaN(args[0])?Number(args[0])-1:0
-			//const ordered = await WorkerInfo.findAll({order: order});
-			const ordered = await sequelize.query("SELECT * FROM `workerinfos` ORDER BY "+order+" DESC", { type: sequelize.QueryTypes.SELECT, model: WorkerInfo});
-			const mapped = ordered.map(x=>[x.cooks, x.delivers, client.users.get(x.id)?client.users.get(x.id).tag:x.username]);
-			let end =  !isNaN(args[1])?Number(args[1]):ordered.length<10?ordered.length:9
-			if (start > end || start > ordered.length || end > ordered.length ) {
-				return message.channel.send("Selection not in range.")
+			let sel = args[2] ? args[2] : "all";
+			if (args[2] && !["all", "cooks", "delivers"].includes(sel)) return message.channel.send("The filter must be 'cooks', 'delivers' or 'all'.");
+			// let order = {"all": [[sequelize.fn('SUM', sequelize.col('cooks'), sequelize.col('delivers')), "DESC"]], "cooks": [["cooks", "DESC"]], "delivers": [["delivers", "DESC"]]}[sel]
+			let order = { "all": "cooks + delivers", "cooks": "cooks", "delivers": "delivers" }[sel];
+			let start = !isNaN(args[0]) ? Number(args[0]) - 1 : 0;
+			// const ordered = await WorkerInfo.findAll({order: order});
+			const ordered = await sequelize.query(`SELECT * FROM \`workerinfos\` ORDER BY ${order} DESC`, { type: sequelize.QueryTypes.SELECT, model: WorkerInfo });
+			const mapped = ordered.map(x => [x.cooks, x.delivers, client.users.get(x.id) ? client.users.get(x.id).tag : x.username]);
+			let end = !isNaN(args[1]) ? Number(args[1]) : ordered.length < 10 ? ordered.length : 9;
+			if (start > end || start > ordered.length || end > ordered.length) {
+				return message.channel.send("Selection not in range.");
 			}
 			if (end - start > 19) return message.channel.send("Please make the number of results less than 20.");
 			const sliced = mapped.slice(start, end);
