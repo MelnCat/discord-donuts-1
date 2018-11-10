c36c8850f4484ab1ba42d1b317b4043a46e87f0c
RaymondW <tnthacker2015@gmail.com>
2018-11-10 15:31:28 -0800

    commit

--- commands/public/help.cmd.js
+++ commands/public/help.cmd.js
@@ -1,28 +1,15 @@
 const DDCommand = require("../../structures/DDCommand.struct");
 const DDEmbed = require("../../structures/DDEmbed.struct");
 const { everyone } = require("../../permissions.js");
-
+const { chunk } = require("../../helpers");	
 module.exports =
 	new DDCommand()
 		.setName("help")
 		.setDescription("Shows help about the bot")
 		.setPermissions(everyone)
 		.setFunction(async(message, args, client) => {
-			let embed = new DDEmbed(client)
-				.setStyle("colorful");
-			let added = [];
-			client.commands.forEach(command => {
-				if (!(command instanceof DDCommand)) return;
-				if (!command.getPermissions(message.member)) return;
-				try {
-					if (!added.includes(command.getName())) {
-						added.push(command.getName());
-						embed.addField(command.getName(), command.getDescription());
-				}
-				} catch (err) {
-					if (!(err instanceof RangeError)) throw err;
-				}
-			});
+			const uniqueCommands = client.commands.array()
+				.filter((val, index, arr) => arr.indexOf(val) === index);
 			chunk(25)(uniqueCommands).forEach(async(section, index, arr) => {
 				const embed = new DDEmbed(client)
 					.setStyle("colorful")
@@ -31,13 +18,10 @@ module.exports =
 				section.forEach(command => {
 					if (!(command instanceof DDCommand)) return;
 					if (!command.getPermissions(message.member)) return;
-
-					embed = new DDEmbed(client)
-						.setStyle("colorful");
-					embed.addField(command.getName(), command.getDescription());
+					embed.addField(`${command.getName()}${command.getAliases().map(x => `, ${x}`).join("")}`, command.getDescription());
 				});
+				await message.author.send(embed);
 			});
 
-			await message.author.send(embed);
-			await message.reply("Check your DM's for my command list");
+			await message.channel.send("Check your DM's for my command list");
 		});
