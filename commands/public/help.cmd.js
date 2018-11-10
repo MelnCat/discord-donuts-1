860a9e61dead690cbce90f33cc1f2ebd660ed1ff
Yamboy1 <mbdolbel@gmail.com>
2018-11-11 08:50:48 +1300

    eslint

--- commands/public/help.cmd.js
+++ commands/public/help.cmd.js
@@ -14,7 +14,7 @@ module.exports =
 
 			chunk(25)(uniqueCommands).forEach(async (section, index, arr) => {
 				const embed = new DDEmbed(client)
-					.setStyle("colorful");
+					.setStyle("colorful")
 					.setTitle(`Discord Donuts Commands (Page ${index+1} of ${arr.length}`)
 				section.forEach(command => {
 					if (!(command instanceof DDCommand)) return;
