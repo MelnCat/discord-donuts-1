b4ee2baabd21445d2bf04890e732376922f986d1
RaymondW <tnthacker2015@gmail.com>
2018-11-10 21:33:52 -0800

    commit

--- commands/public/permissions.cmd.js
+++ commands/public/permissions.cmd.js
@@ -37,7 +37,7 @@ module.exports =
 						.setTitle("Permissions")
 						.setDescription("Your bot permissions.");
 				Object.keys(section).map(bp => {
-					const fun = section[bp];
+					const fun = botperm[bp];
 					const lg = fun(message.member) ? "<:yes:501906738119835649>" : "<:no:501906738224562177>";
 					const txt = pretty(bp.replace(/([A-Z])/g, " $1").toLowerCase());
 					embed.addField(txt, lg, true);
