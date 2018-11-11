8e675d7f57301d8af6b1b6cebcd368801cce7391
Beta <21072460+bvta@users.noreply.github.com>
2018-11-10 23:00:45 -0600

    Update ping.cmd.js

--- commands/public/ping.cmd.js
+++ commands/public/ping.cmd.js
@@ -7,9 +7,9 @@ module.exports =
 		.setName("ping")
 		.setDescription("Fetching timestamp values.")
 		.setPermissions(everyone)
-		.setFunction(async(message, args, client) => {
+		.setFunction(async (message, args, client) => {
 			const startTime = Date.now();
 			const m = await message.channel.send("Fetching responses..");
 			const endTime = Date.now();
-			await m.edit(`Latency is \`${endTime - startTime}ms\`. API latency is \`${client.ws.ping}ms\`.`);
+			await m.edit(`Latency is \`${endTime - startTime}ms\`. API latency is \`${Math.round(client.ws.ping)}ms\`.`);
 		});
