507f4662c29b3914a2300a022542f2c356e9aad4
codefactor-io <support@codefactor.io>
2018-11-11 05:21:15 +0000

    [CodeFactor] Apply fixes to commit 6f9e07b

--- commands/private/claim.cmd.js
+++ commands/private/claim.cmd.js
@@ -11,7 +11,7 @@ module.exports =
 		.setName("claim")
 		.setDescription("Use this to claim donut orders.")
 		.setPermissions(canCook)
-		.setFunction(async (message, args, client) => {
+		.setFunction(async(message, args, client) => {
 			if (message.channel.id !== kitchenChannel) return message.channel.send("<:no:501906738224562177> **You may only utilize this command in the kitchen.**");
 
 			if (!args[0]) return message.channel.send("<:no:501906738224562177> **Invalid arguments provided. Please ensure that you've provided an ID to claim.**");
@@ -23,8 +23,10 @@ module.exports =
 
 			await order.update({ status: 1, claimer: message.author.id });
 
-			const claimEmbed = new MessageEmbed().setColor(0x36393E).setDescription(`🎫 **Your order has been claimed by \`${message.author.tag}\`!\nPlease wait while they process your order.**`).setFooter(message.author.tag, message.author.displayAvatarURL()).setTimestamp()
-			await client.users.get(order.user).send(claimEmbed).catch(e => { return; });
+			const claimEmbed = new MessageEmbed().setColor(0x36393E).setDescription(`🎫 **Your order has been claimed by \`${message.author.tag}\`!\nPlease wait while they process your order.**`)
+.setFooter(message.author.tag, message.author.displayAvatarURL())
+.setTimestamp();
+			await client.users.get(order.user).send(claimEmbed).catch(e => { });
 
 			const embed = new MessageEmbed().setColor(0x36393E).setDescription(`<:yes:501906738119835649> **Order \`${args[0]}\` has been claimed by ${message.author}!**`);
 			await message.channel.send(embed);
