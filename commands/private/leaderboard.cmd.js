c5f8e7278f15e85737f01a8d16b156d2abc691e5
codefactor-io <support@codefactor.io>
2018-11-11 01:25:27 +0000

    [CodeFactor] Apply fixes to commit 2d671cc

--- commands/private/leaderboard.cmd.js
+++ commands/private/leaderboard.cmd.js
@@ -30,7 +30,7 @@ module.exports =
 			const mapped = ordered.map(x => [x.cooks, x.delivers, client.users.get(x.id) ? client.users.get(x.id).tag : x.username, x.id]);
 			let end = !isNaN(args[1]) ? Number(args[1]) : ordered.length < 10 ? ordered.length : 9;
 			if (args[0] || !args[1]) {
-				end = start+1
+				end = start + 1;
 			}
 			if (start > end || start > ordered.length || end > ordered.length) {
 				return message.channel.send("Selection not in range.");
