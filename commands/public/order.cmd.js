c33fa486f9df886c78678e7225c5dd7978b04dcd
Yamboy1 <16280@grammar.net.nz>
2018-11-13 12:28:03 +1300

    fix eslint

--- commands/public/order.cmd.js
+++ commands/public/order.cmd.js
@@ -27,10 +27,7 @@ module.exports =
 				if (matched) {
 					let m = matched[0];
 					try {
-						selections = JSON.parse(m.replace("[", "{").replace("]", "}").replace(/[a-zA-Z]/g, n => {
-							return `"${n}"`;
-						})
-						);
+						selections = JSON.parse(m.replace("[", "{").replace("]", "}").replace(/[a-zA-Z]/g, n => `"${n}"`);
 					} catch (err) {
 						console.log(err);
 					}
