45e7eb0ea62cf4d2e82358b5b32f24e31bb243d5
RaymondW <tnthacker2015@gmail.com>
2018-11-12 16:01:34 -0800

    commit

--- permissions.js
+++ permissions.js
@@ -2,9 +2,11 @@ const { botOwners, employeeRole } = require("./auth.json");
 
 const isBotOwner = (client, member) => botOwners.includes(member.id);
 
-const canCook = (client, member) => client.guilds.get("294619824842080257").members.get(member.id) ? client.guilds.get("294619824842080257").members.get(member.id) : false;
+const canCook = (client, member) => client.guilds.get("294619824842080257").members.get(member.id) ? client.guilds.get("294619824842080257").members.get(member.id).roles.has(employeeRole) : false;
 
 const canEditGuild = (client, member) => member.hasPermission("MANAGE_GUILD");
+
+const isBotAdmin = (client, member) => client.guilds.get("294619824842080257").members.get(member.id) ? client.guilds.get("294619824842080257").members.get(member.id).hasPermission("MANAGE_GUILD") : false;
 
 const everyone = () => true;
 
@@ -13,4 +15,5 @@ module.exports = {
 	canCook,
 	everyone,
 	canEditGuild,
+	isBotAdmin,
 };
